df53c5d1a1b247cc6c23d54a7451ed6eb3760ee8
BlizzardsFlame <ned.r@hotmail.fr>
2019-06-23 19:06:20 +0200

    this is the first version of the app

--- app/src/main/java/com/blizzard/dictionary/MainActivity.java
+++ app/src/main/java/com/blizzard/dictionary/MainActivity.java
@@ -17,10 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        search = (SearchView) findViewById(R.id.search_view);
+        search =  findViewById(R.id.search_view);
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
